6a145f94c2677e3784a3f20c49bff44b0893a58d
gaughanc <celeste@micorb244.med.upenn.edu>
2017-07-19 13:32:01 -0400

    apply to species, indicate field name

--- wb0/management/commands/exporttaxa.py
+++ wb0/management/commands/exporttaxa.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from wb0.models import Genus
+from wb0.models import Genus, Species
 
 # for each genus, print a list of its attributes
 
@@ -7,10 +7,9 @@ class Command(BaseCommand):
     help = "Exports taxa"
 
     def handle(self, *args, **options):
-    	for genus in Genus.objects.all():
-    	    l = []
-    	    for field in Genus._meta.get_fields()[1:]:
-    	    	l.append(getattr(genus, field.name))	
-    	    print(l)
-
-# TODO: apply to species
+    	for Taxon in [Genus, Species]:
+    	    for taxon in Taxon.objects.all():
+    	        l = []
+    	        for field in Taxon._meta.get_fields()[1:]:
+    	    	    l.append(field.name + ": " + getattr(taxon, field.name))	
+    	        print(l)
