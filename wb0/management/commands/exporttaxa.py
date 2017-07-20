d82e206a66474f7b48cb757ac730fc04118bf096
gaughanc <celeste@micorb244.med.upenn.edu>
2017-07-20 14:32:44 -0400

    condense

--- wb0/management/commands/exporttaxa.py
+++ wb0/management/commands/exporttaxa.py
@@ -1,16 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from wb0.models import Genus
 
-# for each genus, print its list of attributes
-
 class Command(BaseCommand):
-    help = "Exports genera"
+    help = "Prints attributes of each genus"
 
     def handle(self, *args, **options):
         fields = Genus._meta.get_fields()[1:]
         print("\t".join(field.name for field in fields))
-        for genus in Genus.objects.all():
-            l = []
-            for field in fields:
-                l.append(getattr(genus, field.name))	
-            print("\t".join(l))
+        for genus in Genus.objects.all():	
+            print("\t".join(getattr(genus, field.name) for field in fields))
